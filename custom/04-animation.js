e800195f1e4d506d1911a9d5a28bfe75a78aa241
Rich Philben Cuer <rich.cuer@dottystylecreative.com>
2024-03-05 10:26:38 -0800

    updates to anim

--- custom/04-animation.js
+++ custom/04-animation.js
@@ -18,8 +18,133 @@
     Animation.prototype.transitionPresets = function (parent = '') {
         const stInstance = ScrollTrigger.create({ start: `top ${startingThreshold}%`, toggleActions: "play none none none", });
         const defaultTweenProps = { duration: 1.2, ease: Power2.easeOut };
-
+        
+        animatedTop();
         animatedCounter();
+        // animatedTopstagger();
+        animatedFade();
+        animatedLeft();
+        animatedRight();
+        animatedLines();
+
+        function animateDirection(type, x, y, alpha) {
+            const elements = gsap.utils.toArray(`${parent} .transition-${type}`);
+
+            if (elements.length) {
+                elements.forEach((el) => {
+                    const customTriggerDistance = $(el).data("trigger-distance");
+                    let triggerPreset = customTriggerDistance ? { ...ScrollTrigger.create({ start: customTriggerDistance }).vars } : { ...stInstance.vars };
+
+                    gsap.set(el, { x, y, autoAlpha: alpha ? 0 : 1 });
+                    gsap.to(el, { ...defaultTweenProps, autoAlpha: 1, x: 0, y: 0, scrollTrigger: { ...triggerPreset, trigger: el } });
+                });
+            }
+        }
+
+        function animatedStagger(type, x, y, alpha) {
+            const wrapper = gsap.utils.toArray(`${parent} .transition-${type}-stagger`);
+
+            if (wrapper.length) {
+                wrapper.forEach((wrap) => {
+                    const mode = $(wrap).data("mode");
+                    if (mode == 'desktop' && smallScreens) return;
+
+                    const customTriggerDistance = $(wrap).data("trigger-distance");
+                    let triggerPreset = customTriggerDistance ? { ...ScrollTrigger.create({ start: customTriggerDistance }).vars } : { ...stInstance.vars };
+                    const children = [...wrap.children];
+
+                    gsap.set(children, { x, y, autoAlpha: alpha ? 0 : 1, clearProps: "all" });
+                    gsap.to(children, { autoAlpha: 1, x: 0, y: 0, stagger: { each: 0.3 }, duration: 0.8, scrollTrigger: { ...triggerPreset, trigger: children, invalidateOnRefresh: true } });
+                });
+            }
+        }
+
+        function animatedTop() {
+            animateDirection("top", 0, 140, false);
+        }
+
+        function animatedFade() {
+            const fadedElements = gsap.utils.toArray(".transition-fade");
+            if (fadedElements.length) {
+                fadedElements.forEach((el) => {
+                    gsap.set(el, { visibility: "hidden" });
+                    gsap.from(el, { ...defaultTweenProps, autoAlpha: 0, scrollTrigger: { trigger: el, ...stInstance.vars, start: "top 80%" } });
+                });
+            }
+
+            const fadedElementsDefault = gsap.utils.toArray(".transition-fade-default");
+            if (fadedElementsDefault.length) {
+                fadedElementsDefault.forEach((el) => {
+                    const customTriggerDistance = $(el).data("trigger-distance");
+                    let triggerPreset = customTriggerDistance ? { ...ScrollTrigger.create({ start: customTriggerDistance }).vars } : { ...stInstance.vars };
+
+                    gsap.set(el, { visibility: "hidden" });
+                    gsap.from(el, { ...defaultTweenProps, autoAlpha: 0, scrollTrigger: { trigger: el, ...triggerPreset } });
+                });
+            }
+        }
+
+        function animatedCounter() {
+            const elementsWithCounterEffect = gsap.utils.toArray(".transition-count");
+
+            const floatNum = (val, decimals, dec_point, thousands_sep) => {
+                dec_point = typeof dec_point !== 'undefined' ? dec_point : '.';
+                thousands_sep = typeof thousands_sep !== 'undefined' ? thousands_sep : ',';
+
+                const parts = val.toFixed(decimals).split('.');
+                parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
+
+                return parts.join(dec_point);
+            }
+
+            const wholeNum = (val) => {
+                const parts = val.toString().split(".");
+                return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
+
+            elementsWithCounterEffect.forEach((el) => {
+                const textContainer = $(el);
+                const targetValue = parseFloat(textContainer.text().replace(/,/g, ''));
+
+                let counter = { val: 0 };
+
+                if (Number.isInteger(targetValue)) {
+                    gsap.to(counter, {
+                        duration: 1.4,
+                        val: targetValue,
+                        onUpdate: function () {
+                            textContainer.text(wholeNum(counter.val));
+                        },
+                        ease: Power3.easeOut,
+                        scrollTrigger: {
+                            trigger: el,
+                            ...stInstance.vars,
+                        },
+                    });
+                } else {
+                    gsap.to(counter, {
+                        duration: 1.4,
+                        val: targetValue,
+                        onUpdate: function () {
+                            textContainer.text(floatNum(counter.val, 2));
+                        },
+                        ease: Power3.easeOut,
+                        scrollTrigger: {
+                            trigger: el,
+                            ...stInstance.vars,
+                        },
+                    });
+                }
+            });
+        }
+
+        function animatedLeft() {
+            animateDirection("left", 60, 0);
+        }
+
+        function animatedRight() {
+            animateDirection("right", -60, 0);
+        }
 
         function animatedCounter() {
             const elementsWithCounterEffect = gsap.utils.toArray(".transition-count");
