aea14f01e90ca8bc68c38a0da6dd9c55a5d40944
Rich Philben Cuer <rich.cuer@dottystylecreative.com>
2024-03-07 13:40:08 -0800

    updates to layout

--- custom/05-carousel.js
+++ custom/05-carousel.js
@@ -7,7 +7,67 @@
     const smallScreens = tablet() || mobile();
 
     Carousel.prototype.init = function () {
+        Carousel.prototype.resourceCarousel();
         app.Global.prototype.refreshScrollTriggers();
+    };
+
+    Carousel.prototype.resourceCarousel = function () {
+        const carousels = $(".resource-carousel");
+        if (!carousels.length) return;
+
+        carousels.each(function () {
+            const self = $(this);
+            const carousel = self.find(".owl-carousel");
+
+            const nativePrev = self.find(".owl-prev");
+            const nativeNext = self.find(".owl-next"); 
+
+            const carouselPrev = self.find(".owl-arrow-l");
+            const carouselNext = self.find(".owl-arrow-r"); 
+
+            const carouselInstance = carousel.owlCarousel({
+                items: 3,
+                autoWidth: true,
+                nav: true,
+                margin: 20,
+                dots: false,
+                smartSpeed: 1000,
+                loop: false,
+                navRewind: false,
+                responsive: {
+                    0: {
+                        margin: 16,
+                    },
+                    768: {
+                        margin: 20,
+                    },
+                }
+            });
+
+            carouselPrev.click(function () {
+                carouselInstance.trigger("next.owl.carousel");
+            });
+
+            carouselNext.click(function () {
+                carouselInstance.trigger("prev.owl.carousel");
+            });
+
+            carouselPrev.addClass("disabled");
+
+            carouselInstance.on("changed.owl.carousel", function (event) {
+                if (nativePrev.hasClass("disabled")) {
+                    carouselPrev.addClass("disabled");
+                } else {
+                    carouselPrev.removeClass("disabled");
+                }
+
+                if (nativeNext.hasClass("disabled")) {
+                    carouselNext.addClass("disabled");
+                } else {
+                    carouselNext.removeClass("disabled");
+                }
+            });
+        });
     };
 
     app.Carousel = Carousel;
