3682b7193a8dcae1584c0b9378a6cddc078233ec
Rich Philben Cuer <rich.cuer@dottystylecreative.com>
2024-03-11 16:08:34 -0800

    updaets to layout

--- custom/04-animation.js
+++ custom/04-animation.js
@@ -188,60 +188,6 @@
             });
         }
 
-        function animatedCounter() {
-            const elementsWithCounterEffect = gsap.utils.toArray(".transition-count");
-
-            const floatNum = (val, decimals, dec_point, thousands_sep) => {
-                dec_point = typeof dec_point !== 'undefined' ? dec_point : '.';
-                thousands_sep = typeof thousands_sep !== 'undefined' ? thousands_sep : ',';
-
-                const parts = val.toFixed(decimals).split('.');
-                parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_sep);
-
-                return parts.join(dec_point);
-            }
-
-            const wholeNum = (val) => {
-                const parts = val.toString().split(".");
-                return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            }
-
-            elementsWithCounterEffect.forEach((el) => {
-                const textContainer = $(el);
-                const targetValue = parseFloat(textContainer.text().replace(/,/g, ''));
-
-                let counter = { val: 0 };
-
-                if (Number.isInteger(targetValue)) {
-                    gsap.to(counter, {
-                        duration: 1.4,
-                        val: targetValue,
-                        onUpdate: function () {
-                            textContainer.text(wholeNum(counter.val));
-                        },
-                        ease: Power3.easeOut,
-                        scrollTrigger: {
-                            trigger: el,
-                            ...stInstance.vars,
-                        },
-                    });
-                } else {
-                    gsap.to(counter, {
-                        duration: 1.4,
-                        val: targetValue,
-                        onUpdate: function () {
-                            textContainer.text(floatNum(counter.val, 2));
-                        },
-                        ease: Power3.easeOut,
-                        scrollTrigger: {
-                            trigger: el,
-                            ...stInstance.vars,
-                        },
-                    });
-                }
-            });
-        }
-
         function animatedLines() {
             const elements = gsap.utils.toArray($(".transition-per-line"));
 
