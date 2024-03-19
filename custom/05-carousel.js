ec004bf20723b08b85c731596cfa09e85eb6d9a1
Rich Philben Cuer <rich.cuer@dottystylecreative.com>
2024-03-19 12:29:08 -0700

    updates to carousel

--- custom/05-carousel.js
+++ custom/05-carousel.js
@@ -141,6 +141,7 @@
                 items: 1,
                 loop: true,
                 margin: 0,
+                smartSpeed: 1000,
                 autoplay: false,
                 slideTransition: 'linear',
             });
